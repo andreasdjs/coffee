49e5043cf77da544184957aa35ad9da5184badf1
andreasdjs <andreas.dagnell@gmail.com>
2016-02-18 19:21:28 +0100

    Planning item listing.

--- routes/list.js
+++ routes/list.js
@@ -3,8 +3,31 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('list');
-});
 
+   // Add function that loads JSON here.
+   // functionName(callback);
+   
+// "title": "Chorongi",
+// "roastery" : "Slöinge",
+
+   // Function to render to jade-page. Passes a JSON-object as a parameter.
+/*
+   function pushContent(obj){
+      res.render('list', {
+         title: 'KAFFESORTER',
+         coffee: obj
+      });
+   }
+*/
+      res.render('list', {
+         title: 'KAFFESORTER',
+         content: 'content'
+//         coffee: obj
+      });
+
+
+//   res.render('list');
+
+});
 
 module.exports = router;
