d03259613433d8f6c073c327235ac9bd0d1a1f77
andreasdjs <andreas.dagnell@gmail.com>
2016-02-18 19:47:08 +0100

    Loading JSON object in list.js and testing iteration in list.jade

--- routes/list.js
+++ routes/list.js
@@ -1,6 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+/* Copy paste from coffee.js */
+
+var fs = require('fs');
+
+function readCoffee(callback) {
+  var fileReadStream = fs.createReadStream('coffee.json');
+  var data = "";
+
+  fileReadStream.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  fileReadStream.on('end', () => {
+      var obj = JSON.parse(data);
+
+      console.log("\nRead JSON file: \n");
+
+      obj.coffee.forEach(function(element){
+        console.log("Id: " + element.id);
+        console.log("Roastery: " + element.roastery);
+        console.log("Title: " + element.title);
+        console.log("Producer: " + element.producer);
+        console.log("Brewing method: " + element.brewingMethod);
+        console.log("About: " + element.about);
+        console.log("\n"); 
+      });    
+
+      callback(obj);
+
+  });
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
@@ -11,20 +43,24 @@ router.get('/', function(req, res, next) {
 // "roastery" : "Slöinge",
 
    // Function to render to jade-page. Passes a JSON-object as a parameter.
-/*
-   function pushContent(obj){
-      res.render('list', {
-         title: 'KAFFESORTER',
-         coffee: obj
+
+	readCoffee(pushContent);
+
+    function pushContent(obj){
+    	res.render('list', {
+        	title: 'KAFFESORTER',
+        	coffee: obj
       });
    }
-*/
+
+/* http://jade-lang.com/reference/iteration/ */
+
+/*
       res.render('list', {
          title: 'KAFFESORTER',
          content: 'content'
-//         coffee: obj
       });
-
+*/
 
 //   res.render('list');
 
